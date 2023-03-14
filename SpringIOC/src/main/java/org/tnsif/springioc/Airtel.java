b76f916207cc4cded0cc8f73bf9044ad6ae64f15
Lenovo <Lenovo@LAPTOP-G7UMRBT2>
2023-03-14 12:49:59 +0530

    first commit

---
+++ SpringIOC/src/main/java/org/tnsif/springioc/Airtel.java
@@ -0,0 +1,15 @@
+package org.tnsif.springioc;
+//implementable class
+public class Airtel implements Sim {
+
+	public void call() {
+		System.out.println("calling using Airtel");
+		
+	}
+
+	public void message() {
+		System.out.println("6G is comming soon.....");
+		
+	}
+
+}
